8248363a39230809800216d2157dfe45a80ad24b
Rafael Bardini <rafael@rbardini.com>
2020-02-24 00:55:08 +0100

    set initial padding from decorator wrapper

--- src/index.tsx
+++ src/index.tsx
@@ -14,8 +14,11 @@ type Props = {
   settings: WrapperSettings;
 }
 
-const state = store({ padding: '' });
-const setPadding = (padding: string) => { state.padding = padding; };
+const state = store({ initialized: false, padding: '' });
+const setPadding = (padding = '') => {
+  if (!state.initialized) state.initialized = true;
+  state.padding = padding;
+};
 
 const bodyClass = 'sb-show-main';
 
@@ -23,11 +26,26 @@ const Story: FC<Props> = view(({
   getStory,
   context,
   settings: {
-    parameters: {
-      disabled = false,
-    } = {},
+    parameters = {},
   },
 }) => {
+  const { disabled = false } = parameters;
+
+  useEffect(() => {
+    // PaddingSelector emits the default selection before this wrapper
+    // can subscribe to the channel, so manually set initial padding on
+    // first render. This workaround should be removed once we can share
+    // state between manager and preview.
+    // See https://github.com/storybookjs/storybook/pull/9079
+    if (!state.initialized) {
+      const defaultValue = Array.isArray(parameters)
+        ? parameters.find((param) => param.default)?.value
+        : undefined;
+
+      setPadding(defaultValue);
+    }
+  }, [parameters]);
+
   useEffect(() => {
     const channel = addons.getChannel();
 
