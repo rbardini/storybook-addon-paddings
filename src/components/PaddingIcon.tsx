9e4ec704601883db1d87802e69c42eafae7a77f3
Rafael Bardini <rafael@rbardini.com>
2020-03-15 15:08:44 +0100

    fix: fix icon size and style

--- src/components/PaddingIcon.tsx
+++ src/components/PaddingIcon.tsx
@@ -2,8 +2,8 @@ import React, { FC } from 'react';
 import { Svg, Path } from './Svg';
 
 const PaddingIcon: FC = () => (
-  <Svg viewBox="0 0 24 24">
-    <Path d="M3,3H21V21H3V3M5,5V19H19V5H5Z M14,10V14H10V10H14M16,8H8V16H16V8Z" />
+  <Svg viewBox="0 0 1024 1024">
+    <Path d="M880 144H144v736h736V144zm40-80c22.092 0 40 17.908 40 40v816c0 22.092-17.908 40-40 40H104c-22.092 0-40-17.908-40-40V104c0-22.092 17.908-40 40-40h816zM224 224v576h576V224H224zm80 80h416v416H304V304z" />
   </Svg>
 );
 
