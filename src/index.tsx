fac10b89214c881138dd2431392632821b54fbab
Rafael Bardini <rafael@rbardini.com>
2020-03-29 17:19:09 +0200

    remove React dependency from decorator

--- src/index.tsx
+++ src/index.tsx
@@ -1,73 +1,51 @@
 /* eslint-disable import/prefer-default-export */
-import React, { FC, useEffect } from 'react';
-import { store, view } from 'react-easy-state';
-import { addons, makeDecorator, WrapperSettings } from '@storybook/addons';
-import { Global } from '@storybook/theming';
+import {
+  addons, makeDecorator, WrapperSettings, StoryWrapper,
+} from '@storybook/addons';
 
-import { EVENTS, PARAM_KEY } from './constants';
+import { DEFAULT_PADDING, EVENTS, PARAM_KEY } from './constants';
+import { getSelectedPadding, isEnabled } from './helpers';
 
-const state = store({ initialized: false, padding: '' });
-const setPadding = (padding = '') => {
-  if (!state.initialized) state.initialized = true;
-  state.padding = padding;
+const state: {
+  initialized: boolean;
+  padding?: string;
+} = {
+  initialized: false,
 };
 
-const bodyClass = 'sb-show-main';
+const setStyle = (padding = DEFAULT_PADDING) => {
+  document.body.style.padding = padding;
+  document.body.style.transition = 'padding .3s';
+};
 
-const Wrapper: FC<{ settings: WrapperSettings }> = view(({
-  children,
-  settings: {
-    parameters = {},
-  },
-}) => {
-  const { disabled = false } = parameters;
+const setPadding = (padding: string) => {
+  state.padding = padding;
+  setStyle(padding);
+};
 
-  useEffect(() => {
-    // PaddingSelector emits the default selection before this wrapper
-    // can subscribe to the channel, so manually set initial padding on
-    // first render. This workaround should be removed once we can share
-    // state between manager and preview.
-    // See https://github.com/storybookjs/storybook/pull/9079
-    if (!state.initialized) {
-      const defaultValue = Array.isArray(parameters)
-        ? parameters.find((param) => param.default)?.value
-        : undefined;
+const render = (settings: WrapperSettings, storyFn: () => ReturnType<StoryWrapper>) => {
+  const { parameters: options = {} } = settings;
+  const channel = addons.getChannel();
 
-      setPadding(defaultValue);
-    }
-  }, [parameters]);
-
-  useEffect(() => {
-    const channel = addons.getChannel();
-
+  if (!state.initialized) {
     channel.on(EVENTS.UPDATE, setPadding);
+    state.initialized = true;
+  }
 
-    return () => channel.off(EVENTS.UPDATE, setPadding);
-  }, []);
+  const currentValue = isEnabled(options) ? state.padding : DEFAULT_PADDING;
+  setStyle(getSelectedPadding(options, currentValue));
 
-  return (
-    <>
-      <Global
-        styles={{
-          [`.${bodyClass}`]: {
-            padding: !disabled && state.padding,
-            transition: 'padding .3s',
-          },
-        }}
-      />
-      {children}
-    </>
-  );
-});
+  return storyFn();
+};
+
+const wrapper: StoryWrapper = (getStory, context, settings) => (
+  render(settings, () => getStory(context))
+);
 
 export const withPaddings = makeDecorator({
   name: 'withPaddings',
   parameterName: PARAM_KEY,
   allowDeprecatedUsage: true,
   skipIfNoParametersOrOptions: true,
-  wrapper: (getStory, context, settings) => (
-    <Wrapper settings={settings}>
-      {getStory(context)}
-    </Wrapper>
-  ),
+  wrapper,
 });
