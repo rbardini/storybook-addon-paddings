d34c771caa25a1da76a721df1b0cc4821472f7d0
Jose Ramirez <jo.chicho@gmail.com>
2020-06-14 17:16:05 +0200

    add function to normalize config

--- src/helpers.tsx
+++ src/helpers.tsx
@@ -2,18 +2,37 @@
 import { WrapperSettings } from '@storybook/addons';
 import { DEFAULT_PADDING } from './constants';
 
-type Option = {
-  name: string;
-  value: string;
-  default?: boolean;
+type Size = { name: string; value: string };
+
+type Option = Size & {default?: boolean}
+
+type Sizes = { [key: string]: Size };
+
+type PaddingConfig = {
+  sizes: Sizes;
+  defaultPadding: string;
 }
 
-type Options = Option[] | WrapperSettings['parameters'];
+type Options = Option[] | PaddingConfig | WrapperSettings['parameters'];
 
-export const isEnabled = (options: Options): options is Option[] => options.length > 0;
+export const normalizeEntries = (options: Options) => (Array.isArray(options)
+  ? options
+  : Object.entries<Size>(options.sizes).map(([key, { name, value }]) => {
+    const isDefault = options.defaultPadding === key;
+
+    return { name, value, default: isDefault };
+  }));
+
+export const isEnabled = (options: Options) => {
+  const items = Array.isArray(options) ? options : Object.entries(options.sizes);
+
+  return items.length > 0;
+};
 
 export const getSelectedPadding = (options: Options, currentValue: string): string => {
-  if (!isEnabled(options)) {
+  const entries = normalizeEntries(options);
+
+  if (!isEnabled(entries)) {
     return DEFAULT_PADDING;
   }
 
@@ -21,9 +40,9 @@ export const getSelectedPadding = (options: Options, currentValue: string): stri
     return currentValue;
   }
 
-  if (options.find(({ value }) => value === currentValue)) {
+  if (entries.find(({ value }) => value === currentValue)) {
     return currentValue;
   }
 
-  return options.find((option) => option.default)?.value ?? DEFAULT_PADDING;
+  return entries.find((option) => option.default)?.value ?? DEFAULT_PADDING;
 };
