4cbde43ded7b3880ce63833a4b45e4665744cae5
Jose Ramirez <jo.chicho@gmail.com>
2020-07-16 01:24:39 +0200

    simplify helper usage

--- src/helpers.tsx
+++ src/helpers.tsx
@@ -17,37 +17,27 @@ type Option = Value & {default?: boolean}
 
 type Options = Option[] | PaddingConfig | WrapperSettings['parameters'];
 
-export const normalizeValues = (options: Options) => {
-  if (Array.isArray(options)) {
-    logger.warn(
-      'Addon Paddings has changed, please migrate to the new recommended configuration.',
-    );
-  }
+export const normalizeValues = (options: Options) => (Array.isArray(options)
+  ? options
+  : Object.entries<Value>(options.values).map(([key, { name, value }]) => {
+    const isDefault = options.default === key;
 
-  return Array.isArray(options)
-    ? options
-    : Object.entries<Value>(options.values).map(([key, { name, value }]) => {
-      const isDefault = options.default === key;
-
-      return { name, value, default: isDefault };
-    });
-};
+    return { name, value, default: isDefault };
+  }));
 
 export const isEnabled = (options: Options) => {
   if (Array.isArray(options)) {
+    logger.warn(
+      'Storybook addon paddings has changed, please migrate to the new recommended configuration.',
+    );
+
     return options.length > 0;
   }
 
-  return !options?.disable || Object.entries(options.values).length > 0;
+  return options && !options?.disable && Object.entries(options.values).length > 0;
 };
 
-export const getSelectedPadding = (options: Options, currentValue: string): string => {
-  const values = normalizeValues(options);
-
-  if (!isEnabled(values)) {
-    return DEFAULT_PADDING;
-  }
-
+export const getSelectedPadding = (values: Option[], currentValue: string): string => {
   if (currentValue === DEFAULT_PADDING) {
     return currentValue;
   }
