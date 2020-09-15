882b32dac7260d14dd4e833190429b1632192bd6
Rafael Bardini <rafael@rbardini.com>
2020-09-15 14:10:19 +0200

    Replace deprecated addParameters and addDecorator APIs

--- example/.storybook/preview.js
+++ example/.storybook/preview.js
@@ -1,12 +1,11 @@
-import { addDecorator, addParameters } from '@storybook/react';
 import { withPaddings } from 'storybook-addon-paddings';
 
-addDecorator(withPaddings);
+export const decorators = [withPaddings];
 
-addParameters({
+export const parameters = {
   paddings: [
     { name: 'Small', value: '16px' },
     { name: 'Medium', value: '32px', default: true },
     { name: 'Large', value: '64px' },
   ],
-});
+};
