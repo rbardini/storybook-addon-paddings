9cf8a651ffee7235554cc65eb1abe4f6f7421c24
Rafael Bardini <rafael@rbardini.com>
2021-05-16 00:31:59 +0200

    Set styles for story and docs view modes

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,64 @@
-import { StoryWrapper, useMemo } from '@storybook/addons';
+import { StoryWrapper, useEffect, useMemo } from '@storybook/addons';
 
 import { DEFAULT_PADDING, PARAM_KEY } from './constants';
 import { normalizeValues, getSelectedPadding } from './helpers';
 
-const setStyle = (padding = DEFAULT_PADDING) => {
-  document.body.style.margin = '0';
-  document.body.style.padding = padding;
-  document.body.style.transition = 'padding .3s';
+const setStyle = (selector: string, css: string) => {
+  const existingStyle = document.getElementById(selector);
+
+  if (existingStyle) {
+    if (existingStyle.innerHTML !== css) {
+      existingStyle.innerHTML = css;
+    }
+  } else {
+    const style = document.createElement('style');
+    style.setAttribute('id', selector);
+    style.innerHTML = css;
+
+    document.head.appendChild(style);
+  }
 };
 
 export const withPaddings: StoryWrapper = (getStory, context) => {
-  const { globals, parameters } = context;
+  const { id, globals, parameters, viewMode } = context;
   const globalsSelectedPadding = globals[PARAM_KEY]?.value;
   const paddingsConfig = parameters[PARAM_KEY];
+  const selector =
+    viewMode === 'docs' ? `#anchor--${id} .docs-story > div` : '.sb-show-main';
 
   // eslint-disable-next-line react-hooks/rules-of-hooks
-  useMemo(() => {
-    const selectedPadding = getSelectedPadding(
-      normalizeValues(paddingsConfig),
-      globalsSelectedPadding,
-    );
-    setStyle(selectedPadding);
-  }, [paddingsConfig, globalsSelectedPadding]);
+  const selectedPadding = useMemo(
+    () =>
+      getSelectedPadding(
+        normalizeValues(paddingsConfig),
+        globalsSelectedPadding,
+      ) ?? DEFAULT_PADDING,
+    [paddingsConfig, globalsSelectedPadding],
+  );
+
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const paddingStyles = useMemo(
+    () => `
+      ${selector} {
+        margin: 0;
+        padding: ${selectedPadding} !important;
+        transition: padding .3s;
+      }
+
+      ${selector} .innerZoomElementWrapper > div {
+        border-width: 0 !important;
+      }
+    `,
+    [selector, selectedPadding],
+  );
+
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  useEffect(() => {
+    const selectorId =
+      viewMode === 'docs' ? `addon-paddings-docs-${id}` : `addon-paddings`;
+
+    setStyle(selectorId, paddingStyles);
+  }, [id, viewMode, paddingStyles]);
 
   return getStory(context);
 };
